b52e3e4cdcb33edd82edffb98b059289efb252aa
Divyansh Agarwal <77184239+nikkuAg@users.noreply.github.com>
2024-11-01 11:35:43 +0530

    Update settings.py

--- forex_vision/settings.py
+++ forex_vision/settings.py
@@ -110,6 +110,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
+STATIC_ROOT = 'media/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
